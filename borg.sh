b45caf4040d2a5d16d49c59eee8b660a9ee44487
Alex <modem7@gmail.com>
2023-02-24 19:08:28 +0000

    Updating scripts

---
+++ borg.sh
@@ -0,0 +1,34 @@
+#/bin/bash
+
+# Prerequesites
+sudo pip install -U build cibuildwheel PyYAML requests
+sudo apt install jq wget curl
+
+# Variables
+export PKGNAME="borgbackup"
+export PKGVER=$(grep "${PKGNAME}" requirements.txt | cut -d '=' -f 3 | tr -d '\012\015')
+export PKGURL=$(curl -s "https://pypi.org/pypi/${PKGNAME}/${PKGVER}/json" | jq -r '.urls[].url')
+export CIBW_BEFORE_ALL='apk add -Uu acl-dev alpine-sdk attr-dev attr-dev bzip2-dev fuse-dev g++ libcrypto1.1 libffi-dev libssl1.1 libxxhash linux-headers lz4-dev musl-dev ncurses-dev openssl-dev py3-msgpack py3-packaging py3-pip py3-setuptools py3-setuptools_scm py3-wheel py3-xxhash python3-dev readline-dev sqlite-dev tree xxhash-dev xz-dev zlib-dev zstd-dev && pip install -U pkgconfig pip setuptools wheel'
+export CIBW_ARCHS_LINUX='x86_64 aarch64'
+export CIBW_BUILD='cp311-musllinux_*'
+
+# Load Multiarch settings
+docker run --rm --privileged tonistiigi/binfmt
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
+# Create directories
+mkdir -p sdist wheelhouse
+
+# Download
+wget -c $PKGURL -q -O - | tar -xz -C ./sdist/
+
+# Build
+cibuildwheel --platform linux --output-dir wheelhouse sdist/$PKGNAME-$PKGVER
+
+# Remove working files
+rm -rf ./sdist/$PKGNAME*
+
+# Remove leftover Docker images
+docker rmi tonistiigi/binfmt
+docker rmi multiarch/qemu-user-static
+docker rmi $(docker images | grep quay.io/pypa/ | awk "{print \$3}")
---
+++ cloudsmith.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+# Variables
+export CLOUDSMITH_API="${CLOUDSMITH_API:-}"
+
+# Apply extra cron if it's set
+if [ -n CLOUDSMITH_API ]
+then
+    echo "Enter Cloudsmith API token: "
+    read CLOUDSMITH_API
+fi
+
+# Prerequesites
+sudo pip install -U cloudsmith-cli
+
+# Upload to Cloudsmith
+for f in $PWD/wheelhouse/*.whl
+  do
+    echo "Processing $f file..."
+    # take action on each file. $f store current file name
+    cloudsmith push python -W -k $CLOUDSMITH_API --no-republish modem7/wheels "$f"
+  done
